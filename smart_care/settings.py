5a6789affd65f01ce8a163b5ecdd24f1e0be552d
Sakilshikdar <sakilshikdar2504@gmil.com>
2024-01-13 17:27:48 +0600

    host-allowed

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-g^r9^7w)ofxpxszp5db)+)acmxq=ei)9989!rxd_h#s1%!p*mm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
